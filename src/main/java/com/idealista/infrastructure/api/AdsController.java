b93651e9eb9c34d35662cc251c900ce9526fbf1a
Rubén Briones <rubenyes@gmail.com>
2019-10-30 11:59:09 +0100

    First commit checking that everything compile and the server works ok.

--- src/main/java/com/idealista/infrastructure/api/AdsController.java
+++ src/main/java/com/idealista/infrastructure/api/AdsController.java
@@ -3,11 +3,19 @@ package com.idealista.infrastructure.api;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdsController {
 
+
+	@RequestMapping(value = "/",method = RequestMethod.GET)
+	public String pruebahola() {
+		return "hola ruben";
+	}
+	
     //TODO añade url del endpoint
     public ResponseEntity<List<QualityAd>> qualityListing() {
         //TODO rellena el cuerpo del método
